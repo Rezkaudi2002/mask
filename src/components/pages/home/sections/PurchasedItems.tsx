52c99ceeb84396abf798f8b43bb9b9ce6d05d753
Hakam aldeen Alkhadraa <hakamaldeenkh@outlook.com>
2025-01-20 21:53:55 +0300

    create purchasedItems section

--- src/components/pages/home/sections/PurchasedItems.tsx
+++ src/components/pages/home/sections/PurchasedItems.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 
-const PurchasedItems: React.FC = () => {
-    return (
-        <section>
-            Purchased Items
-        </section>
-    )
+interface IPurchasedItemsProps {}
+
+const PurchasedItems = ({}: IPurchasedItemsProps) => {
+  return (
+    <section className="relative py-[50px] px-5">
+      <Image
+        src="/bg-items.jpg"
+        alt="banner-right-bg"
+        fill
+        className="object-cover opacity-10"
+      />
+      <div className="absolute h-full w-full left-0 top-0 bg-[#FFE0E3] -z-10"></div>
+      <h2 className="text-[30px] leading-[45px] font-black text-center font-noto bg-gradient-to-r from-light-red to-dark-red bg-clip-text text-transparent">
+        買取品目一覧
+      </h2>
+    </section>
+  );
 };
 
 export default PurchasedItems;
