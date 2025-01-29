897a76b0b50d8b0c781b758216faa39f1c4aec26
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-01-29 14:49:16 +0300

    clean Inquiry component.

--- src/components/common/sections/Inquiry.tsx
+++ src/components/common/sections/Inquiry.tsx
@@ -1,144 +1,20 @@
 "use client";
-
 import RadioGroup from "../../pages/home/components/RadioGroup";
 import InputField from "../../pages/home/components/InputField";
 import SelectField from "../../pages/home/components/SelectField";
 import ImageUpload from "../../pages/home/components/ImageUpload";
 import Image from "next/image";
-import emailjs from "@emailjs/browser";
-import React, { useState } from "react";
-import Swal from "sweetalert2";
+import React from "react";
+import { useFormHandler } from "@/hooks/useFormHandler";
 
 const Inquiry = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    phonePermission: "",
-    usageType: "",
-    invoiceRegistration: "",
-    provideRegistrationNumber: "",
-    city: "",
-    product_info: "",
-    inquiry_source: "",
-    product_details: "",
-    product_condition: "",
-    image: null as string | null,
-    additional_notes: "",
-  });
-
-  const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener("mouseenter", Swal.stopTimer);
-      toast.addEventListener("mouseleave", Swal.resumeTimer);
-    },
-  });
-
-  const handleInputChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-    >
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
-
-  const handleImageChange = (image: string | null) => {
-    setFormData((prevData) => ({ ...prevData, image }));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // EmailJS Configuration
-    const serviceID = "service_p3to9nt";
-    const systemTemplateID = "template_xf3foxn"; // For sending to your system
-    const welcomeTemplateID = "template_id6emd5"; // For sending to the client
-    const publicKey = "0xF8VQGwM-H1P-NVr";
-
-    // Email parameters for your system (internal email)
-    const emailParams = {
-      ...formData,
-      image: formData.image ? formData.image : "No Image Provided",
-      to_email: "hakamha8@gmail.com", // Your system email (hardcoded)
-    };
-
-    // Email parameters for client (welcome email)
-    const welcomeParams = {
-      ...formData,
-      image: formData.image ? formData.image : "No Image Provided",
-      name: formData.name,
-      user_email: formData.email,
-    };
-
-    // // Email parameters for the client (welcome email)
-    // const welcomeParams = {
-    //   to_name: formData.name, // Client's name
-    //   to_email: formData.email, // Client's email
-    //   message: `Welcome, ${formData.name}! Thank you for registering. Here are your submitted details:`,
-    //   form_data: JSON.stringify(formData, null, 2), // Include all form data
-    // };
-
-    // Send email to your system
-    emailjs
-      .send(serviceID, systemTemplateID, emailParams, publicKey)
-      .then(() => {
-        // Reset form data after successful email
-        setFormData({
-          name: "",
-          email: "",
-          phone: "",
-          phonePermission: "",
-          usageType: "",
-          invoiceRegistration: "",
-          provideRegistrationNumber: "",
-          city: "",
-          product_info: "",
-          inquiry_source: "",
-          product_details: "",
-          product_condition: "",
-          image: null,
-          additional_notes: "",
-        });
-
-        // Toast notification
-        Toast.fire({
-          icon: "success",
-          title: "メールが送信されました！",
-        });
-
-        // Send email for client
-        emailjs
-          .send(serviceID, welcomeTemplateID, welcomeParams, publicKey)
-          .then(() => {
-            // Toast notification for welcome email
-            Toast.fire({
-              icon: "success",
-              title: "Welcome email sent to client!",
-            });
-          })
-          .catch((error) => {
-            // Handle welcome email errors
-            Toast.fire({
-              icon: "error",
-              title: "Error sending welcome email: " + error.message,
-            });
-          });
-      })
-      .catch((error) => {
-        // Handle email sending errors
-        Toast.fire({
-          icon: "error",
-          title: "送信中にエラーが発生しました: " + error.message,
-        });
-      });
-  };
-
+  const {
+    formData,
+    isSubmitting,
+    handleInputChange,
+    handleImageChange,
+    handleSubmit,
+  } = useFormHandler();
   return (
     <section id="inquiry" className="py-[50px] lg:py-[60px] px-[20px] lg:px-0 bg-[url(/images/home-page/dot-bg-results.svg)] bg-auto font-noto">
       <h2 className="mb-[40px] md:mb-[42px] lg:mb-[50px] text-[30px] md:text-[40px] lg:text-[60px] leading-[45px] md:leading-[60px] lg:leading-[90px] font-black bg-gradient-to-r from-light-red to-dark-red bg-clip-text text-transparent text-center">
@@ -308,13 +184,19 @@ const Inquiry = () => {
           type="submit"
           className="w-[90%] lg:w-[60%] py-[12px] mx-auto text-[24px] leading-[36px] text-center text-white font-black rounded flex items-start justify-center gap-x-4 gradient-red"
         >
-          送信する
-          <Image
-            src="/images/icons/arrow-right-red.svg"
-            alt="arrow-right-red"
-            width={32}
-            height={32}
-          />
+          {isSubmitting === true ? (
+            "送信..."
+          ) : (
+            <>
+              送信する
+              <Image
+                src="/images/icons/arrow-right-red.svg"
+                alt="arrow-right-red"
+                width={32}
+                height={32}
+              />
+            </>
+          )}
         </button>
       </form>
     </section>
