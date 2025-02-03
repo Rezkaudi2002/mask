17552b1af4c36b09098d0e6020bff31cfab7fbf8
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2025-02-03 22:16:48 +0900

    text styling and translation

--- src/components/pages/home/components/PurchaseItemsCategoryCard.tsx
+++ src/components/pages/home/components/PurchaseItemsCategoryCard.tsx
@@ -15,8 +15,8 @@ const PurchaseItemsCategoryCard = ({
     <button
       key={id}
       className={`
-    px-[16px] py-[11px] lg:p-[15px] text-center border-[2px] relative w-fit md:max-w-[27%] 
-    text-[14px] lg:text-[18px] leading-[18px] lg:leading-[24px] font-noto font-black min-w-[290px] lg:min-w-[360px]
+      px-[16px] py-[11px] lg:p-[15px] text-center border-[2px] relative w-fit md:max-w-[27%] 
+      text-[14px] lg:text-[18px] leading-[18px] lg:leading-[24px] font-noto min-w-[290px] lg:min-w-[360px] font-semibold
     ${
       activeCategory === title
         ? "border-[#990E1C] text-white gradient-red text-shadow-red"
