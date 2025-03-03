f928e66afc8240f6caf9a65888cc0e254e3b2be4
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2025-03-03 23:07:14 +0900

    text updated

--- src/components/pages/home/sections/PurchasedItems.tsx
+++ src/components/pages/home/sections/PurchasedItems.tsx
@@ -54,7 +54,7 @@ const PurchasedItems = () => {
             href={`/products/${selectedCategory}`}
             className="block w-full md:w-[50%] lg:w-[30%] px-[16px] py-[11px] lg:p-[15px] text-center text-[14px] lg:text-[18px] leading-[18px] lg:leading-[24px] font-noto font-semibold border-[2px] border-[#990E1C] text-white gradient-red text-shadow-red"
           >
-            Show More
+            もっと見る
           </Link>
         </div>
       )}
