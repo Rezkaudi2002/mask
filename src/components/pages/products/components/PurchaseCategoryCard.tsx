eea48aba87085ae41453d9ef3c19a3ce30f7cfa1
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-03-05 14:39:11 +0300

    Add the image and title for the purchase history sections

--- src/components/pages/products/components/PurchaseCategoryCard.tsx
+++ src/components/pages/products/components/PurchaseCategoryCard.tsx
@@ -5,13 +5,10 @@ interface IPurchaseCategoryCardProps {
   title: string;
 }
 
-const PurchaseCategoryCard = ({
-  title,
-  image,
-}: IPurchaseCategoryCardProps) => {
+const PurchaseCategoryCard = ({ title, image }: IPurchaseCategoryCardProps) => {
   return (
     <div className="w-[47%] md:w-[30%] lg:w-[22%]">
-      <div className="p-[20px_24px_60px_24px] lg:p-[25px_58px_65px_58px] min-h-[240px] lg:min-h-[258px] bg-white relative">
+      <div className="min-h-[240px] lg:min-h-[258px] bg-white relative">
         <div className="w-full flex justify-center items-center">
           <Image
             src={image}
@@ -22,7 +19,7 @@ const PurchaseCategoryCard = ({
             className="w-auto h-[140px] lg:w-auto lg:h-[190px]"
           />
         </div>
-        <p className="max-h-[100px] px-[10px] lg:px-[6px] py-[7px] lg:py-[12px] absolute bottom-0 left-0 w-full text-[#B81122] text-center font-semibold text-[20px] lg:text-[30px] leading-[27px] lg:leading-[30px] overflow-hidden text-ellipsis line-clamp-2">
+        <p className="px-[10px] lg:px-[6px] py-[7px] lg:py-[12px] w-full text-[#B81122] text-center font-semibold text-[14px] lg:text-[20px] leading-[22px] lg:leading-[35px] text-ellipsis">
           {title}
         </p>
       </div>
