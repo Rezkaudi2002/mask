9ec8b92fcddf77b20f54b902cfd8e2a5c439d41a
rezkaudi <audirezk@gmail.com>
2025-03-07 19:51:32 +0300

    end build page for 農業機械 category

--- src/content/categories/categories.ts
+++ src/content/categories/categories.ts
@@ -1,6 +1,8 @@
 import category1 from './category1.json'
 import category2 from './category2.json'
+import category3 from './category3.json'
 
-const categories = [category1,category2]
+
+const categories = [category1, category2, category3]
 
 export default categories
