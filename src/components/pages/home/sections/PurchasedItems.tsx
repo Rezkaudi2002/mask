1a1a866b93bd78ba95a7f1414ff4974550c7afbf
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-03-09 21:40:01 +0300

    pull from main and solve confilect

--- src/components/pages/home/sections/PurchasedItems.tsx
+++ src/components/pages/home/sections/PurchasedItems.tsx
@@ -48,21 +48,14 @@ const PurchasedItems = () => {
         ))}
       </div>
       {/* show more button */}
-      {[
-        "工作機械・精密加工機械",
-        "自動車整備機械",
-        "農業機械",
-        "金属加工機械・板金機械 (建築板金機械)",
-      ].includes(selectedCategory) && (
-        <div className="w-full mt-10 md:mt-12 flex justify-center">
-          <Link
-            href={`/products/${selectedCategory}`}
-            className="block w-full md:w-[50%] lg:w-[30%] px-[16px] py-[11px] lg:p-[15px] text-center text-[14px] lg:text-[18px] leading-[18px] lg:leading-[24px] font-noto font-semibold border-[2px] border-[#990E1C] text-white gradient-red text-shadow-red"
-          >
-            もっと見る
-          </Link>
-        </div>
-      )}
+      <div className="w-full mt-10 md:mt-12 flex justify-center">
+        <Link
+          href={`/products/${selectedCategory}`}
+          className="block w-full md:w-[50%] lg:w-[30%] px-[16px] py-[11px] lg:p-[15px] text-center text-[14px] lg:text-[18px] leading-[18px] lg:leading-[24px] font-noto font-semibold border-[2px] border-[#990E1C] text-white gradient-red text-shadow-red"
+        >
+          もっと見る
+        </Link>
+      </div>
     </section>
   );
 };
--- src/content/categories/categories.ts
+++ src/content/categories/categories.ts
@@ -2,8 +2,9 @@ import category1 from './category1.json'
 import category2 from './category2.json'
 import category3 from './category3.json'
 import category4 from './category4.json'
+import category5 from './category5.json'
 
 
-const categories = [category1, category2, category3, category4]
+const categories = [category1, category2, category3, category4, category5 ]
 
 export default categories
