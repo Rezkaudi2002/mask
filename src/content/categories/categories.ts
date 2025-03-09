8bf0dbba72ce4b033561bf567d1a37ea236144d1
rezkaudi <audirezk@gmail.com>
2025-03-09 15:29:13 +0300

    end build page for 発電機・溶接機・エアコンプレッサ category

--- src/content/categories/categories.ts
+++ src/content/categories/categories.ts
@@ -2,7 +2,10 @@ import category1 from './category1.json'
 import category2 from './category2.json'
 import category3 from './category3.json'
 
+import category5 from './category5.json'
 
-const categories = [category1, category2, category3]
+
+
+const categories = [category1, category2, category3, category5]
 
 export default categories
