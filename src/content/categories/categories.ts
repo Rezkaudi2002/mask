e9ff9a1b7787f0d7c80d9bb589101005c5feb7be
rezkaudi <audirezk@gmail.com>
2025-03-10 16:50:59 +0300

    end build page for 住宅工事工具・大工機械 category

--- src/content/categories/categories.ts
+++ src/content/categories/categories.ts
@@ -3,8 +3,9 @@ import category2 from './category2.json'
 import category3 from './category3.json'
 import category4 from './category4.json'
 import category5 from './category5.json'
+import category7 from './category7.json'
 
 
-const categories = [category1, category2, category3, category4, category5]
+const categories = [category1, category2, category3, category4, category5, category7]
 
 export default categories
