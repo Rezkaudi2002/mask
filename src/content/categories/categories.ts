d8359e4cd5b69648b56b986a51e7a191be6abe81
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-03-11 10:56:51 +0300

    start 管工事機械・工具 cateogry

--- src/content/categories/categories.ts
+++ src/content/categories/categories.ts
@@ -3,9 +3,10 @@ import category2 from './category2.json'
 import category3 from './category3.json'
 import category4 from './category4.json'
 import category5 from './category5.json'
+import category6 from './category6.json'
 import category7 from './category7.json'
 
 
-const categories = [category1, category2, category3, category4, category5, category7]
+const categories = [category1, category2, category3, category4, category5, category6, category7]
 
 export default categories
