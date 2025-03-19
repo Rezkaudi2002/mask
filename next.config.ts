914b9cc16e41e0496ca3b84f7e48a85570dafe9f
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-03-19 22:01:24 +0300

    build その他の買取対応 category page

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: { 
-    formats: ['image/avif', 'image/webp'], 
-    }
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['mac-hadis.s3.ap-northeast-1.amazonaws.com'],
+  }
 };
 
 export default nextConfig;
