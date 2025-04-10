ef8a19f75ce012c636bde1f6ebb8999af716da1e
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-04-10 16:02:54 +0300

    change a ui for purchseCard if product have details

--- src/components/pages/home/components/PurchaseItemsCard.tsx
+++ src/components/pages/home/components/PurchaseItemsCard.tsx
@@ -18,12 +18,12 @@ const PurchaseItemsCard = ({
     <Link
       href={haveDetails ? `/products/${categoryId}/${title}` : ""}
       className={`block p-[20px_24px_60px_24px] lg:p-[25px_58px_65px_58px] min-h-[240px] lg:min-h-[258px] border-[2px] border-[#B81122] rounded-[4px] bg-white relative ${
-        haveDetails && "cursor-pointer"
+        haveDetails ? "cursor-pointer" : "cursor-default"
       }`}
     >
       {haveDetails === true && (
-        <span className="absolute left-2 top-4 bg-[#b81122] p-1 text-sm text-white">
-          Have Details
+        <span className="absolute left-2 top-4 bg-green-600 p-1 text-sm text-white rounded-sm">
+          詳細あり
         </span>
       )}
       <div className="w-full flex justify-center items-center">
@@ -36,8 +36,14 @@ const PurchaseItemsCard = ({
           className="w-auto h-[140px] lg:w-auto lg:h-[190px]"
         />
       </div>
-      <p className="max-h-[100px] px-[10px] lg:px-[6px] py-[7px] lg:py-[12px] absolute bottom-0 left-0 w-full bg-[#B81122] flex justify-center items-center text-white text-center font-semibold text-[10px] lg:text-[20px] leading-[27px] lg:leading-[30px] overflow-hidden text-ellipsis line-clamp-2">
-        {title}
+      <p className="max-h-[100px] px-[10px] lg:px-[6px] py-[7px] lg:py-[12px] absolute bottom-0 left-0 w-full bg-[#B81122] flex justify-between items-center text-white font-semibold text-[10px] lg:text-[20px] leading-[27px] lg:leading-[30px] overflow-hidden text-ellipsis line-clamp-2">
+        <span className="flex-1 text-center">{title}</span>
+        <Image
+          width={20}
+          height={20}
+          src="https://mac-hadis.s3.ap-northeast-1.amazonaws.com/icons/info-circle.svg"
+          alt="info-icon"
+        />
       </p>
     </Link>
   );
