c2a6e6cb482d8e0f481c1918e2e13d8bd0ce0e34
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-05-07 21:43:42 +0300

    add a images for スポット溶接機 product details

--- src/content/product_details/products.ts
+++ src/content/product_details/products.ts
@@ -1,6 +1,7 @@
 import product1 from './自動車整備機械/整備リフト.json'
 import product2 from './自動車整備機械/タイヤチェンジャー.json'
+import product3 from './自動車整備機械/スポット溶接機.json'
 
-const products = [product1, product2]
+const products = [product1, product2, product3]
 
 export default products
