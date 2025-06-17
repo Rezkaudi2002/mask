a302c10d018eb040d10482e86229b9ca5fab913c
Rezkaudi <audirezk@gmail.com>
2025-06-17 14:16:57 +0300

    test6

--- src/components/common/sections/Footer.tsx
+++ src/components/common/sections/Footer.tsx
@@ -83,7 +83,7 @@ const Footer: React.FC = () => {
 
         {/* copyright */}
         <span className="text-xs text-[#CA999B]">
-          Copyright © ハディズ All rights reserved.tes6
+          Copyright © ハディズ All rights reserved.test7
         </span>
       </div>
     </footer>
