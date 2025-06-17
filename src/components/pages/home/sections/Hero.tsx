9938c5ff4b9611f848c5993657caa360b319cdc1
nadasobeh22 <sobehnada2@gmail.com>
2025-06-17 19:14:54 +0300

    test

--- src/components/pages/home/sections/Hero.tsx
+++ src/components/pages/home/sections/Hero.tsx
@@ -10,17 +10,17 @@ const Hero: React.FC = () => {
       >
         {/* Background wrapper to ensure correct positioning */}
         <div className="absolute inset-0 -z-30">
-          {/* Mobile Background */}
+          {/* Mobile Background - */}
           <Image
             src="https://mac-hadis.s3.ap-northeast-1.amazonaws.com/home-page/hero-section/hero-background-mobile.webp"
             alt="Company's legacy image"
-            sizes="100vw"
-            quality={100}
+            sizes="(max-width: 1023px) 100vw, 0px"
+            quality={60}
             fill
             priority
             className="object-cover object-center block lg:hidden"
           />
-          {/* Desktop Background */}
+          {/* Desktop Background -*/}
           <Image
             src="https://mac-hadis.s3.ap-northeast-1.amazonaws.com/home-page/hero-section/hero-background.webp"
             alt="Company's legacy image"
@@ -32,14 +32,15 @@ const Hero: React.FC = () => {
           />
         </div>
         <div className="flex lg:w-1/2 2xl:min-w-[1000px] items-start 2xl:items-center justify-center h-full 2xl:h-3/4 space-y-2 lg:space-y-7 flex-col pt-7 2xl:pt-20 sm:pl-12 pb-4">
-          {/* image 1  */}
+          {/* image 1 */}
           <div className="text-hero relative xl:w-[810px] xl:h-[64px] w-[335px] h-[38px] sm:h-[50px] lg:h-[70px] z-10">
             <Image
               src="https://mac-hadis.s3.ap-northeast-1.amazonaws.com/home-page/hero-section/hero-1.webp"
               alt="Company's legacy image"
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 1200px"
+              sizes="(max-width: 640px) 335px, (max-width: 768px) 400px, (max-width: 1200px) 50vw, 1200px"
               fill
               priority
+              quality={70}
             />
           </div>
           {/* image 2 */}
@@ -47,10 +48,10 @@ const Hero: React.FC = () => {
             <Image
               src="https://mac-hadis.s3.ap-northeast-1.amazonaws.com/home-page/hero-section/hero-2.webp"
               alt="Hero promotional banner with details"
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 1200px"
+              sizes="(max-width: 640px) 340px, (max-width: 768px) 400px, (max-width: 1200px) 50vw, 1200px"
               fill
               priority
-              quality={100}
+              quality={70}
             />
           </div>
 
